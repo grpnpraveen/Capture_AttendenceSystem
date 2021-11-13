6baa31763b2f3206b620098fa93c09304167d78d
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-11-13 09:17:24 +0530

    commit

--- Main.py
+++ Main.py
@@ -75,33 +75,35 @@ app = Flask(__name__, template_folder='./templates')
 def gen_frames():  # generate frame by frame from camera
     global capture,regisno,camera
     while True:
-        success, frame = camera.read() 
+        success, frame = camera.read()
+        frame2=frame
         if success:   
             if(capture):
                 capture=0
-                now = datetime.datetime.now()
+                # now = datetime.datetime.now()
 
-                p = os.path.sep.join(['shots', "{}.png".format(str(pin)+"_"+str(regisno).lower())])
-                cv2.imwrite(p, frame)
-                camera.release() 
+                # p = os.path.sep.join(['shots', "{}.png".format(str(pin)+"_"+str(regisno).lower())])
+                # cv2.imwrite(p, frame)
+                camera.release()  
         try:
             train_faceLoc = face_recognition.face_locations(frame)[0]
             cv2.rectangle(frame,(train_faceLoc[3],train_faceLoc[0]),(train_faceLoc[1],train_faceLoc[2]),(255,255,0),2)
-            encoding = face_recognition.face_encodings(frame)[0]
-
-            output_for_user=find_compare(regisno,frame)
+        except:
+            pass  # write face not recognised
+        try:
+            ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        except Exception as e:
+            pass
+        try:
+            output_for_user=find_compare(regisno,frame2)
             print(output_for_user)
         except:
-            print("Face not recognised properly!")
-            pass  # write face not recognised
+            pass
  
-            try:
-                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            except Exception as e:
-                pass
+
 
 @app.route('/video_feed')
 def video_feed():
