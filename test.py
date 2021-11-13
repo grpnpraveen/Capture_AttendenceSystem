4432557985e3d998d4c0ce0deb4369edaa5ab84d
Gali Ravi Praveen <grpnpraveen@gmail.com>
2021-11-13 12:05:04 +0530

    updated db

--- test.py
+++ test.py
@@ -4,21 +4,21 @@ def get_database():
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
     CONNECTION_STRING = "mongodb://localhost:27017"
     client = MongoClient(CONNECTION_STRING)
-    return client['Attendence']
+    return client['BML']
 
 dbname = get_database()
 
 todo=dbname["PA_SUB_DDMNYY_HHMM_UNIQCODE"]
-# for i in range(9):
-#     item_1 = {
-#     "_id":"REGNO"+str(i),
-#     "present":"1/0",
-#     }
-#     todo.insert_one(item_1)
-id="REGNO0"
-item_details=todo.find()
-print(todo)
-for item in item_details:
-    # This does not give a very readable output
-    if item["_id"]==id:
-        print(item)
+for i in range(9):
+    item_1 = {
+    "_id":"REGNO"+str(i),
+    "present":"1/0",
+    }
+    todo.insert_one(item_1)
+# id="REGNO0"
+# item_details=todo.find()
+# print(todo)
+# for item in item_details:
+#     # This does not give a very readable output
+#     if item["_id"]==id:
+#         print(item)
